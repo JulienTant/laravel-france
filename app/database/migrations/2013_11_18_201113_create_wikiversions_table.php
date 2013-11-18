bb607dbbb43288bf81d81a36c793465b5f6e88b8
Julien Tant <julien@ao6-labs.eu>
2013-11-18 21:15:06 +0100

    Migrations wiki

---
+++ app/database/migrations/2013_11_18_201113_create_wikiversions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWikiversionsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('wiki_versions', function(Blueprint $table)
+		{
+            $table->increments('id');
+
+            $table->integer('wiki_page_id')->unsigned();
+            $table->integer('version')->unsigned();
+            $table->text('content');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('wiki_versions');
+	}
+
+}
