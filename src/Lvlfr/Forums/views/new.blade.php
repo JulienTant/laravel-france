ee78449b69a8deadcb92277a293a105139ebb026
Julien Tant <julien@laravel.fr>
2014-07-27 08:19:57 -0500

    correction new

--- src/Lvlfr/Forums/views/new.blade.php
+++ src/Lvlfr/Forums/views/new.blade.php
@@ -28,14 +28,14 @@
             )
         ) }}
 
-        <div class="control-group @if($errors->has('topic_title'))error@endif">
+        <div class="control-group @if($errors->has('topic_title')) error @endif">
             <div class="controls">
                 {{ Form::text('topic_title', Input::old('topic_title', null), array('placeholder'=>'Veuillez insérer un titre', 'class'=>'span12', "autofocus"=>"autofocus")) }}
                 {{ $errors->first('topic_title', '<span class="help-inline">Veuillez insérer un titre</span>') }}
             </div>
         </div>
 
-        <div class="control-group @if($errors->has('topic_content'))error@endif" style="margin-top:20px">
+        <div class="control-group @if($errors->has('topic_content')) error @endif" style="margin-top:20px">
             <div class="controls">
                 {{ Form::textarea('topic_content', Input::old('topic_content', null), array('class'=>'span12')) }}
                 {{ $errors->first('topic_content', '<span class="help-inline">Veuillez insérer un message</span>') }}
