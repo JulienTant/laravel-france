518150d0fba0786bbe25ff2fb0daba805cb545c2
Julien Tant <julien@laravel.fr>
2014-08-25 09:56:41 -0500

    gestion des très vieux liens de doc avec des v3 et v4 qui arrivent encore sur le site

--- src/Lvlfr/Documentation/Controller/DocumentationController.php
+++ src/Lvlfr/Documentation/Controller/DocumentationController.php
@@ -15,6 +15,12 @@ class DocumentationController extends \BaseController
             $version = Config::get('LvlfrDocumentation::docs.defaultVersion');
             return \Redirect::action('\Lvlfr\Documentation\Controller\DocumentationController@showDocs', [$version]);
         }
+
+        if (\Str::startsWith($version, ['v'])) {
+            return \Redirect::action('\Lvlfr\Documentation\Controller\DocumentationController@showDocs', [str_replace('v', '', $version)], 301);
+        }
+
+
         $versionConfig = Config::get('LvlfrDocumentation::docs.versions')[(string)$version];
 
         if ($document === null) {
@@ -22,16 +28,22 @@ class DocumentationController extends \BaseController
         }
 
         $data = array(
-            'document'   => $document,
-            'menu'     => $versionConfig['menu']
+            'document' => $document,
+            'menu' => $versionConfig['menu']
         );
 
         try {
-            array_walk($data, function (&$raw) use ($versionConfig) {
-                $path = base_path().Config::get('LvlfrDocumentation::docs.path', '/docs') . '/' . $versionConfig['path'];
-                $raw = File::get($path."/{$raw}.md");
-                $raw = markdownThis($raw);
-            });
+            array_walk(
+                $data,
+                function (&$raw) use ($versionConfig) {
+                    $path = base_path() . Config::get(
+                            'LvlfrDocumentation::docs.path',
+                            '/docs'
+                        ) . '/' . $versionConfig['path'];
+                    $raw = File::get($path . "/{$raw}.md");
+                    $raw = markdownThis($raw);
+                }
+            );
 
         } catch (Exception $e) {
             App::abort(404);
@@ -57,7 +69,7 @@ class DocumentationController extends \BaseController
                 $data['next'] = $link;
                 break;
             } else {
-                $foundCurrent = (str_replace('/'.$version.'/', '', $link['URI']) == $document);
+                $foundCurrent = (str_replace('/' . $version . '/', '', $link['URI']) == $document);
 
                 if (!$foundCurrent) {
                     $data['prev'] = (!$link['title']) ? null : $link;
