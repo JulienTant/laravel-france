e238303dd65751e54f9178a21e1b20ddbb664eaf
TANT Julien <julien@ao6-labs.eu>
2014-12-25 16:26:50 +0100

    new menu

--- app/views/base/layout.blade.php
+++ app/views/base/layout.blade.php
@@ -7,7 +7,7 @@
 
     <link rel="stylesheet" href="/css/normalize.css">
     <link rel="stylesheet" href="/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/app.css?v=20141211">
+    <link rel="stylesheet" href="/css/app.css?v=20141225">
     @yield('add_css')
 </head>
 <body class="@yield('page_class') preload">
@@ -33,35 +33,31 @@
             </h1>
             <ul class="menu">
                 <li class="toggle"><a href="javascript:void(0);"><i class="icon-reorder"></i> Menu</a></li>
-                <li @if(str_is('*docs.*', Request::root()))class='active'@endif><a href="{{ action('\Lvlfr\Documentation\Controller\DocumentationController@showDocs') }}"><i class="icon-book"></i> Documentation</a></li>
+                <li @if(str_is('*forums.*', Request::root()))class='active'@endif><a href="{{ action('\Lvlfr\Forums\Controller\HomeController@index', null, true) }}">Forums</a></li>
+                <li @if(str_is('*docs.*', Request::root()))class='active'@endif><a href="{{ action('\Lvlfr\Documentation\Controller\DocumentationController@showDocs') }}">Documentation</a></li>
+                <li @if(str_is('*wiki.*', Request::root()))class='active'@endif><a href="{{ action('\Lvlfr\Wiki\Controller\HomeController@index', null, true) }}">Wiki</a></li>
+                <li @if(Request::is('*irc/index'))class='active'@endif><a href="{{ action('Lvlfr\Website\Controller\IrcController@getIndex', null, true) }}">IRC</a></li>
 
-                <li @if(Request::is('*irc') || str_is('*forums.*', Request::root()) || str_is('*wiki.*', Request::root()))class='active'@endif>
-                    <a href="#"><i class="icon-group"></i> Communauté</a>
+                <li @if(Request::is('contact*'))class='active'@endif><a href="{{ action('Lvlfr\Website\Controller\ContactController@getIndex', null, true) }}"> Contact</a></li>
+            </ul>
+            <ul class="menu menu-account">
+                <li class="account">
+                @if(Auth::check())
+                    <a href="{{ action('Lvlfr\Login\Controller\ProfileController@index') }}"><i class="icon-user"></i> {{ Auth::user()->username }}</a>
                     <ul>
-                        <li @if(str_is('*forums.*', Request::root()))class='active'@endif><a href="{{ action('\Lvlfr\Forums\Controller\HomeController@index', null, true) }}">Forums</a></li>
-                        <li @if(str_is('*wiki.*', Request::root()))class='active'@endif><a href="{{ action('\Lvlfr\Wiki\Controller\HomeController@index', null, true) }}">Wiki</a></li>
-                        <li @if(Request::is('*irc/index'))class='active'@endif><a href="{{ action('Lvlfr\Website\Controller\IrcController@getIndex', null, true) }}">IRC</a></li>
+                        <li><a href="{{ action('Lvlfr\Login\Controller\ProfileController@index') }}">Profil</a></li>
+                        <li><a href="{{ action('Lvlfr\Login\Controller\LoginController@logout', null, true) }}">Déconnexion</a></li>
                     </ul>
                 </li>
-                <li @if(Request::is('contact*'))class='active'@endif><a href="{{ action('Lvlfr\Website\Controller\ContactController@getIndex', null, true) }}"><i class="icon-envelope"></i> Contact</a></li>
-                @if(Auth::check())
-                    <li class="account">
-                        <a href="{{ action('Lvlfr\Login\Controller\ProfileController@index') }}"><i class="icon-user"></i> {{ Auth::user()->username }}</a>
-                        <ul>
-                            <li><a href="{{ action('Lvlfr\Login\Controller\ProfileController@index') }}">Profil</a></li>
-                            <li><a href="{{ action('Lvlfr\Login\Controller\LoginController@logout', null, true) }}">Déconnexion</a></li>
-                        </ul>
-                    </li>
                 @else
-                    <li class="account">
-                        <a href="#"><i class="icon-user"></i> Connexion</a>
-                        <ul>
-                            <li><a href="{{ action('Lvlfr\Login\Controller\LoginController@login', array('Google'), true) }}">Google</a></li>
-                            <li><a href="{{ action('Lvlfr\Login\Controller\LoginController@login', array('twitter'), true) }}">Twitter</a></li>
-                            <li><a href="{{ action('Lvlfr\Login\Controller\LoginController@login', array('GitHub'), true) }}">Github</a></li>
-                        </ul>
-                    </li>
+                    <a href="#"><i class="icon-user"></i> Connexion</a>
+                    <ul>
+                        <li><a href="{{ action('Lvlfr\Login\Controller\LoginController@login', array('Google'), true) }}">Google</a></li>
+                        <li><a href="{{ action('Lvlfr\Login\Controller\LoginController@login', array('twitter'), true) }}">Twitter</a></li>
+                        <li><a href="{{ action('Lvlfr\Login\Controller\LoginController@login', array('GitHub'), true) }}">Github</a></li>
+                    </ul>
                 @endif
+                </li>
             </ul>
         </nav>
     </header>
