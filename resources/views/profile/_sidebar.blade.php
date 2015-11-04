8502321027c0bc0744b7792debc2ef180a0c6cd4
Julien Tant <julien@craftyx.fr>
2015-11-04 22:55:20 +0100

    add more preferences for the forums

--- resources/views/profile/_sidebar.blade.php
+++ resources/views/profile/_sidebar.blade.php
@@ -11,6 +11,9 @@
                 <a href="{{ route('profile.change-avatar') }}">Changer mon avatar</a>
             </li>
             <li>
+                <a href="{{ route('profile.change-avatar') }}">Changer mon email</a>
+            </li>
+            <li>
                 <a href="{{ route('profile.forums') }}">Préférences des forums</a>
             </li>
         </ul>
--- resources/views/profile/forums.blade.php
+++ resources/views/profile/forums.blade.php
@@ -4,8 +4,6 @@
 @section('profile_content')
     <h2>Préférences des forums</h2>
 
-    <p>Vous trouverez ici une série de préférences liées aux forumf :</p>
-
     {!! Form::model(Auth::user(), ['class' => 'Form']) !!}
 
     @if($errors->has())
@@ -17,12 +15,38 @@
     @endif
 
 
-    <div class="Form__Row">
-        <label class="Form__Row__Label">
-            {!! Form::checkbox('preference[see_last_message]', 1, isset($preferences['see_last_message']) && $preferences['see_last_message'], ['class' => 'Form__Row__Control']) !!}
-            Voir le dernier message lors du clic sur un topic depuis la liste des sujets
-        </label>
-    </div>
+    <fieldset class="Form__Fieldset">
+        <legend class="Form__Fieldset__Legend">Listing des topics</legend>
+
+        <div class="Form__Row">
+            <label class="Form__Row__Label">
+                {!! Form::checkbox('preference[see_last_message]', 1, isset($preferences['see_last_message']) && $preferences['see_last_message'], ['class' => 'Form__Row__Control']) !!}
+                Voir le dernier message lors du clic sur un sujet depuis la page d'accueil des forums
+            </label>
+        </div>
+    </fieldset>
+
+    <fieldset class="Form__Fieldset">
+        <legend class="Form__Fieldset__Legend">Alertes</legend>
+
+        <p>
+            <strong>Rappel : </strong>Votre adresse email est : {{ Auth::user()->email }}. <a href="{{ route('profile.change-avatar') }}">Changer</a>
+        </p>
+
+        <div class="Form__Row">
+            <label class="Form__Row__Label">
+                {!! Form::checkbox('preference[watch_created_topic]', 1, isset($preferences['watch_created_topic']) && $preferences['watch_created_topic'], ['class' => 'Form__Row__Control']) !!}
+                Surveiller automatiquement les sujets que je crée.
+            </label>
+        </div>
+
+        <div class="Form__Row">
+            <label class="Form__Row__Label">
+                {!! Form::checkbox('preference[watch_new_reply_send_email]', 1, isset($preferences['watch_new_reply_send_email']) && $preferences['watch_new_reply_send_email'], ['class' => 'Form__Row__Control']) !!}
+                M'envoyer un email lorsqu'un nouveau message est posté sur un sujet que je surveille
+            </label>
+        </div>
+    </fieldset>
 
     <div class="Form__Row Form__Row--Buttons">
         <button type="submit" class="Button--Submit">Enregistrer</button>
