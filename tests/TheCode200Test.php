91bfbf30228b62f8c5343b08c70c8ef0e9c11c1e
Julien Tant <julien@craftyx.fr>
2015-12-05 23:40:51 +0100

    add test

--- tests/TheCode200Test.php
+++ tests/TheCode200Test.php
@@ -13,6 +13,23 @@ class TheCode200Test extends TestCase
         $this->visit('/')->see('Laravel France')->seeStatusCode(200);
     }
 
+    public function testForumCategory()
+    {
+        DB::table('forums_categories')->delete();
+        DB::table('forums_categories')->insert([
+            'id' => '1',
+            'slug' => 'my-slug',
+            'name' => 'Name',
+            'description' => 'this is for the tests',
+            'background_color' => 'red',
+            'font_color' => 'white',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ]);
+
+        $this->visit('/c/my-slug')->see('this is for the tests')->seeStatusCode(200);
+    }
+
     public function testSlackPageisOk()
     {
         $this->visit('/slack')->see('Laravel France')->seeStatusCode(200);
