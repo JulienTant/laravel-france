31cedd6587cc371400d2fc47cdd8b9d71ba3360b
Julien Tant <julien@craftyx.fr>
2016-08-16 22:48:23 +0200

    only send mail to valid email addresses

--- app/Listeners/SendEmailToWatchersWhenForumsMessagesPostedListener.php
+++ app/Listeners/SendEmailToWatchersWhenForumsMessagesPostedListener.php
@@ -38,8 +38,8 @@ class SendEmailToWatchersWhenForumsMessagesPostedListener
 
         /** @var ForumsWatch $watcher */
         foreach (ForumsWatch::mailable()->with('user')->whereForumsTopicId($topic->id)->where('user_id', '<>', $poster->id)->get() as $watcher) {
-            if ($watcher->user->getForumsPreferencesItem('watch_new_reply_send_email')) {
 
+            if ($watcher->user->getForumsPreferencesItem('watch_new_reply_send_email') && filter_var($watcher->user->email, FILTER_VALIDATE_EMAIL)) {
                 $user = $watcher->user;
 
                 $parameters = [
@@ -47,6 +47,7 @@ class SendEmailToWatchersWhenForumsMessagesPostedListener
                     'author'        => $poster->username,
                     'message_id'    => $event->getMessage()->id
                 ];
+
 
                 $this->mailer->queue('forums.email.watch', $parameters, function (Message $message) use ($user, $parameters) {
                     $message->to($user->email, $user->username);
