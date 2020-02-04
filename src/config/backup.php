dffdd6a73366c54bc7b2a23ac42c7fee100a4bbd
Julien Tant <julien@craftyx.fr>
2020-02-04 19:14:24 +0100

    change backup name

--- src/config/backup.php
+++ src/config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'backup/laravel-france'),
+        'name' => env('APP_NAME', 'backup-laravel-france'),
 
         'source' => [
 
@@ -163,7 +163,7 @@ return [
      */
     'monitor_backups' => [
         [
-            'name' => env('APP_NAME', 'backup/laravel-france'),
+            'name' => env('APP_NAME', 'backup-laravel-france'),
             'disks' => ['local', 'dropbox'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
