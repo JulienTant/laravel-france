195def7c8e00195d1103aee90e3456fb8fdbf603
Julien Tant <julien@craftyx.fr>
2020-02-25 14:28:39 +0100

    update meta files

--- src/.phpstorm.meta.php
+++ src/.phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-02-22 16:13:01.
+    * Generated on 2020-02-25 13:27:40.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
